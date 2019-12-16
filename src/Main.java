c509cc931eeb1756e9692617b47cbbc5242ef6e8
Țurcanu Andrei <andrei.turcanu@unifun.com>
2019-12-16 23:20:58 +0200

    Used generics in ArrayList , one is type integer another is type String

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,7 +8,6 @@ public class Main {
         Glass <Lemonade>glass1 = new Glass();
 
         Glass<String> glass2 = new Glass<>();
-
 
         glass2.liquid = "Tomato juice";//this glass if of a type string
 
@@ -16,6 +18,25 @@ public class Main {
         Lemonade lemonade = glass1.liquid;//i can make a Lemonade object and give him value from glass1.liquid , becouse glass.liquid is of type Lemonade.
 
         System.out.println(glass2.liquid);
+
+    //here i created two different ArrayList , one is composed from integers and another is composed from strings
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<String>  list2 = new ArrayList<>();
+
+        list1.add(2);//adding element in list1 of type int
+        list2.add("Hello");//adding element in list 2 of type String
+
+        //display list1
+        for(String i:list2){
+            System.out.println(i);
+        }
+
+        //display list2
+        for(int i:list1){
+            System.out.println(i);
+        }
+
+
 
     }
 }
